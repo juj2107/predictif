caf6b07d89675469477e64c4f8ec94736afd4251
Juliette Duprez <jduprez@insa-lyon.fr>
2026-05-16 17:09:31 +0200

    consultation si connecte seulement

--- TD2/src/main/java/web/modele/CommencerConsultationAction.java
+++ TD2/src/main/java/web/modele/CommencerConsultationAction.java
@@ -6,7 +6,9 @@ package web.modele;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import metier.modele.Client;
 import metier.modele.Consultation;
+import metier.modele.Medium;
 import metier.service.Service;
 
 /**
@@ -22,7 +24,18 @@ public class CommencerConsultationAction extends Action {
     public void execute(HttpServletRequest request) {
         HttpSession session = request.getSession();
         String type = (String)session.getAttribute("type");
-        Consultation consultation = Service.demanderConsultation(client, medium);
+        if (type.equals("client")) {
+            Integer id = (Integer)session.getAttribute("userId");
+            Client client = Service.recupererClientParId(id);
+            String idMedium = request.getParameter("medium");
+            Medium medium = Service.recupererMediumParId(Integer.valueOf(idMedium));
+            Consultation consultation = Service.demanderConsultation(client, medium);
+            request.setAttribute("consultation", consultation);
+            request.setAttribute("connecte", true);
+        } else {
+            request.setAttribute("connecte", false);
+        }
+        
     }
     
     
--- TD2/src/main/java/web/vue/CommencerConsultationSerialisation.java
+++ TD2/src/main/java/web/vue/CommencerConsultationSerialisation.java
@@ -4,9 +4,13 @@
  */
 package web.vue;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObjectBuilder;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import metier.modele.Consultation;
 
 /**
  *
@@ -19,7 +23,28 @@ public class CommencerConsultationSerialisation extends Serialisation {
 
     @Override
     public void appliquer(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Consultation consultation = (Consultation) request.getAttribute("consultation");
+        Boolean connecte = (Boolean) request.getAttribute("connecte");
+        System.out.println("serialisation "+ consultation);
+        JsonObjectBuilder jsonContainer = Json.createObjectBuilder();
+        if (connecte == true) {
+            jsonContainer.add("connecte",true);
+            if(consultation == null){
+                jsonContainer.add("consultation",false);
+            }else{
+                jsonContainer.add("consultation",true);
+            }
+        } else {
+            jsonContainer.add("connecte",false);
+        }
+        
+
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print(jsonContainer.build().toString());
+        out.close();
     }
     
 }
