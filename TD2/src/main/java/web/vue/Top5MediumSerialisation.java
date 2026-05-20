adca3b37271f3b930f70d7703027632838831897
Juliette Duprez <jduprez@insa-lyon.fr>
2026-05-20 17:57:13 +0200

    statistiques

---
+++ TD2/src/main/java/web/vue/Top5MediumSerialisation.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package web.vue;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import metier.modele.Medium;
+
+/**
+ *
+ * @author jduprez
+ */
+public class Top5MediumSerialisation extends Serialisation{
+
+    @Override
+    public void appliquer(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        List<Medium> mediums = (List<Medium>) request.getAttribute("mediums");      
+        System.out.println("serialisation "+ mediums);
+        JsonObjectBuilder jsonContainer = Json.createObjectBuilder();
+        JsonArrayBuilder jsonListeMediums = Json.createArrayBuilder();
+
+        for (Medium medium : mediums) {
+            JsonObjectBuilder jsonMedium = Json.createObjectBuilder();
+            jsonMedium.add("medium", medium);
+
+            jsonListeMediums.add(jsonMedium);
+        }
+        jsonContainer.add("mediums", jsonListeMediums);
+        
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print(jsonContainer.build().toString());
+        out.close();
+    }
+    
+}
